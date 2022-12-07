3902e71a98541204cea577333083053d877f1eca
Marco Munizaga <git@marcopolo.io>
2022-12-06 16:50:23 -0800

    Try CI

--- multidim-interop/versions.ts
+++ multidim-interop/versions.ts
@@ -13,27 +13,27 @@ export type Version = {
 }
 
 export const versions: Array<Version> = [
-    {
-        id: "rust-v0.49.0",
-        containerImageID: rustv049.imageID,
-        transports: ["tcp", "ws"],
-        secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "rust-v0.50.0",
-        containerImageID: rustv050.imageID,
-        transports: ["ws", "tcp", "quic-v1"],
-        secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "go-v0.24.0",
-        containerImageID: gov024.imageID,
-        transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
+    // {
+    //     id: "rust-v0.49.0",
+    //     containerImageID: rustv049.imageID,
+    //     transports: ["tcp", "ws"],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "rust-v0.50.0",
+    //     containerImageID: rustv050.imageID,
+    //     transports: ["ws", "tcp", "quic-v1"],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "go-v0.24.0",
+    //     containerImageID: gov024.imageID,
+    //     transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
     {
         id: "go-v0.23.4",
         containerImageID: gov023.imageID,
